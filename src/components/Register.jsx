47cfbfedebc4386aa20dce331c99cb090f827b9e
in-imitable <atul69030@gmail.com>
2023-12-03 15:12:45 +0530

    first commit

---
+++ src/components/Register.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import { BASE_URL, REGISTER_ENDPOINT } from '../utils/api';
+
+const Register = () => {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleRegister = async () => {
+    try {
+      const response = await axios.post(`${BASE_URL}${REGISTER_ENDPOINT}`, { username, email, password });
+      console.log(response.data);
+    } catch (error) {
+      console.error(error.response.data);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Register</h2>
+      <input type="text" placeholder="Username" onChange={(e) => setUsername(e.target.value)} />
+      <input type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
+      <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+      <button onClick={handleRegister}>Register</button>
+    </div>
+  );
+};
+
+export default Register;
---
+++ src/utils/api.js
@@ -0,0 +1,4 @@
+export const BASE_URL = 'http://localhost:5000';
+export const REGISTER_ENDPOINT = '/auth/register';
+export const LOGIN_ENDPOINT = '/auth/login';
+
